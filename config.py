cb349773b61b45e936ccf0851038d79acdf0932e
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-11-14 01:25:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
-from base64 import b64decode
-
-# Decode API ID and Hash from base64
-API_ID = b64decode("MjUzMjQ1ODE=").decode()  # Your API ID
-API_HASH = b64decode("MDhmZWVlNWVlYjZmYzBmMzFkNWYyZDIzYmIyYzMxZDA=").decode()  # Your API Hash
-
-# Decode BOT Token from base64 (optional step)
-BOT_TOKEN = 7211603667:AAFEWFN-aWIu7wV0-fUu_ou1-PfT5rZOTdQ  # Your Bot Token
+API_ID = "20787644"
+API_HASH = "9dada820698e8a5fdd5e6cc78fac8567"
+BOT_TOKEN = "7211603667:AAFEWFN-aWIu7wV0-fUu_ou1-PfT5rZOTdQ"
