ac7c8db6cb487736e45a36698335dac14d74fb2e
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-11-14 12:55:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-from pyrogram import Client
-import handlers  # This will register handlers without needing `bot`
+from handlers import bot
 
-# Import configuration
-from config import API_ID, API_HASH, BOT_TOKEN
-
-# Initialize Pyrogram Client
-app = Client(
-    "GroupFetchAi",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
+if __name__ == "__main__":
+    bot.run()
 
 print("Bot is starting...")
 
-# Start the bot
-app.run()
