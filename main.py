fb0724f5ab12486e13b0f0b4c24f60699ee01ef4
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-11-14 17:46:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 from handlers import bot
-from aiogram import Bot, Dispatcher, executor
-import asyncio
-
-from config import BOT_TOKEN
-
-# Initialize Aiogram Bot
-aio_bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(aio_bot)
-
-print("Bot is starting...")
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    bot.run()
 
-    # Start Pyrogram bot
-    loop.create_task(bot.start())  # Use 'start()' instead of 'run()' for compatibility with asyncio
-
-    # Start Aiogram bot
-    from handlers import setup_aiogram_handlers  # Ensure your handlers are imported here
-    setup_aiogram_handlers(dp)
-    executor.start_polling(dp, skip_updates=True)
+print("Bot is starting...")
